1fa76023869dfc4169beb75e38ab5ac417a66684
Sina Haseli <sina.haseli@gmail.com>
2020-12-23 18:26:10 +0330

    adding-files

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faDribbble } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
   return (
@@ -9,201 +11,131 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <section>
+          <ul class="social">
+            <li class="li1">
+              <a class="ease-all" href="#">
+                <FontAwesomeIcon icon={faDribbble}></FontAwesomeIcon>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+                <span class="hide-text">dribble</span>
+              </a>
+            </li>
+            <li class="li2">
+              <a class="ease-all" href="#">
+                <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+                <span class="hide-text">Twitter</span>
+              </a>
+            </li>
+          </ul>
+        </section>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        section {
+          width: 200px;
+          margin: 0 auto;
         }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        span {
+          text-indent: 100%;
+          white-space: nowrap;
+          overflow: hidden;
+          display: block;
         }
 
         a {
-          color: inherit;
-          text-decoration: none;
+          -webkit-transition: all 400ms ease-in-out;
+          -moz-transition: all 400ms ease-in-out;
+          -o-transition: all 400ms ease-in-out;
+          -ms-transition: all 400ms ease-in-out;
+          transition: all 400ms ease-in-out;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+        /* Social Icons */
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
+        ul {
+          height: 10rem;
+          list-style-type: none;
           margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
+          padding: 0;
           text-align: center;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
+        ul li a {
+          display: inline-block;
+          float: left;
+          width: 40px;
+          height: 40px;
+          margin-right: 1rem;
+          text-align: center;
+          padding-top: 13px;
+
+          -webkit-border-radius: 20px;
+          -moz-border-radius: 20px;
+          -o-border-radius: 20px;
+          -ms-border-radius: 20px;
+          border-radius: 20px;
+
+          -webkit-box-shadow: inset 0 0 0 40px #495057;
+          -moz-box-shadow: inset 0 0 0 40px #495057;
+          -o-box-shadow: inset 0 0 0 40px #495057;
+          -ms-box-shadow: inset 0 0 0 40px #495057;
+          box-shadow: inset 0 0 0 40px #495057;
+
+          color: #fff;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        ul li a:hover {
+          -webkit-box-shadow: inset 0 0 0 3px #fff;
+          -moz-box-shadow: inset 0 0 0 3px #fff;
+          -o-box-shadow: inset 0 0 0 3px #fff;
+          -ms-box-shadow: inset 0 0 0 3px #fff;
+          box-shadow: inset 0 0 0 3px #fff;
+
+          color: #fff;
         }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
+        ul li:nth-child(2) a:hover {
+          background: #55acee;
         }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        ul li:nth-child(1) a:hover {
+          background: #e36397;
         }
       `}</style>
 
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+        * {
+          -moz-box-sizing: border-box;
+          -webkit-box-sizing: border-box;
+          box-sizing: border-box;
         }
 
-        * {
-          box-sizing: border-box;
+        html {
+          -moz-text-size-adjust: 100%;
+          -ms-text-size-adjust: 100%;
+          -webkit-text-size-adjust: 100%;
+          -o-text-size-adjust: 100%;
+          text-size-adjust: 100%;
+        }
+
+        body {
+          font-family: sans-serif;
+          padding: 2rem 4rem;
+          background-color: #f5f5f5;
+        }
+
+        section {
+          width: 200px;
+          margin: 0 auto;
+        }
+
+        .hide-text {
+          text-indent: 100%;
+          white-space: nowrap;
+          overflow: hidden;
+          display: block;
         }
       `}</style>
     </div>
-  )
+  );
 }
